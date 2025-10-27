f96f8737e0ab658ec626917415fbe70a61e94dbc
Flávio Félix <fau_felix30@hotmail.com>
2025-10-27 19:41:27 -0300

    Done operadores aritméticos, atribuição e  relacionais

---
+++ curso_python/src/operadores/aritmeticos.py
@@ -0,0 +1,11 @@
+a = 10
+b = 6
+
+print(4 + 7)  # Valores literais
+print(a + b)  # Soma
+print(a - b)  # Subtracao
+print(a * b)  # Multiplicacao
+print(a / b)  # Divisao
+print(a // b)  # Divisao inteira
+print(a % b)  # Resto da divisao
+print(a**b)  # Exponenciacao
